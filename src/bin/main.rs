6fa6308303c17caa95c860b7c6ff67657d639979
lliryk <94886092+lliryk@users.noreply.github.com>
2022-05-17 19:55:41 -0400

    Initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::process;
+use std::path::Path;
+
+use floyd_dithering::config::Config;
+use floyd_dithering::config::Extension;
+use floyd_dithering::bit_map::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Encountered an error processing config: {}", err);
+        process::exit(1);
+    });
+
+    println!("{:?}", config);
+
+    match config.extension {
+        Extension::BMP => {
+            let mut bit_map = BitMap::new(&config.filename).unwrap_or_else(|err| {
+                eprintln!("Error while processing bitmap file: {}", err);
+                process::exit(1);
+            });
+            bit_map.debug();
+
+            // Transform the image
+            bit_map.transform();
+
+            let size = bit_map.save(Path::new("edited.bmp")).unwrap_or_else(|err| {
+                eprintln!("Error while saving bitmap file: {}", err);
+                process::exit(1);
+            });
+            
+            println!("Wrote {} bytes", size);
+            
+        }
+    }
+
+}
